fe0a2efe343b02694300d6dd71c5c8c1a0652dc3
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-11-25 11:59:37 -0300

    Fixing install

--- service_install.sh
+++ service_install.sh
@@ -2,12 +2,14 @@
 
 echo -e "starting service install"
 
+systemctl stop sensor-publisher-go
+
 mv /usr/local/sensor-publisher-go/sensor-publisher-go.service /etc/systemd/system/sensor-publisher-go.service
 
 systemctl daemon-reload
 
-/boot/dietpi/dietpi-services enable sensor-publisher-go
-/boot/dietpi/dietpi-services start sensor-publisher-go
-/boot/dietpi/dietpi-services status sensor-publisher-go
+systemctl enable sensor-publisher-go.service
+systemctl start sensor-publisher-go.service
+systemctl status sensor-publisher-go.service
 
 echo -e "finish service install"
