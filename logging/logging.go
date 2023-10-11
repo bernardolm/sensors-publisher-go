6ac86b543b5995f2734ad6e0b4da55fd2d096f98
Bernardo Loureiro <bernardo.lou@gmail.com>
2023-10-11 15:23:47 -0300

    Getting log level from env

--- logging/logging.go
+++ logging/logging.go
@@ -2,9 +2,17 @@ package logging
 
 import (
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func Init() {
-	// set log level
-	log.SetLevel(log.DebugLevel)
+	ll := viper.GetString("LOG_LEVEL")
+
+	var level log.Level = log.InfoLevel
+
+	if ll != "" {
+		level, _ = log.ParseLevel(ll)
+	}
+
+	log.SetLevel(level)
 }
